8723dbf85208369f5046ee66deadd753db0b561f
rackylaguerta <rackylaguerta081410@gmail.com>
2024-04-22 20:29:16 +0800

    update storage name

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "asg" {
 }
 
 resource "azurerm_storage_account" "swisspine_storage" {
-  name                     = "swisspine_storage"
+  name                     = "swisspinestorage"
   resource_group_name      = data.azurerm_resource_group.asg.name
   location                 = data.azurerm_resource_group.asg.location
   account_tier             = "Standard"
